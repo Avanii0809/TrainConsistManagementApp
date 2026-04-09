ac3a1a6d13a9222bedd8bf85b9d281fe89940577
Avanii0809 <as080906@gmail.com>
2026-04-09 15:08:53 +0530

    UC4

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,29 +1,47 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
+import java.util.List;
 
-public class UseCase3TrainConsistMgmt {
+public class UseCase4TrainConsistMgmt {
 
     public static void main(String[] args) {
 
         System.out.println("===========================================");
-        System.out.println("   UC3 - Track Unique Bogie IDs");
+        System.out.println("   UC4 - Maintain Ordered Bogie Consist");
         System.out.println("===========================================\n");
 
-        Set<String> bogies = new HashSet<>();
+        List<String> trainConsist = new LinkedList<>();
 
-        bogies.add("B6101");
-        bogies.add("B6102");
-        bogies.add("B6103");
-        bogies.add("B6104");
+        addBogie(trainConsist, "B1");
+        addBogie(trainConsist, "B2");
+        addBogie(trainConsist, "B3");
 
-        bogies.add("B6101");
-        bogies.add("B6102");
+        addBogie(trainConsist, "B2");
 
-        System.out.println("Unique Bogie IDs:");
-        for (String bogie : bogies) {
-            System.out.println(bogie);
+        System.out.println("After adding bogies: " + trainConsist);
+
+        String newBogie = "B1.5";
+        if (!trainConsist.contains(newBogie)) {
+            trainConsist.add(1, newBogie);
+        }
+        System.out.println("After inserting at position 1: " + trainConsist);
+
+        if (!trainConsist.isEmpty()) {
+            trainConsist.remove(0);
         }
 
-        System.out.println("\nTotal unique bogies: " + bogies.size());
+        if (!trainConsist.isEmpty()) {
+            trainConsist.remove(trainConsist.size() - 1); // rear
+        }
+
+        System.out.println("After removing front & rear: " + trainConsist);
+
+        System.out.println("\nFinal Train Consist: " + trainConsist);
+    }
+    public static void addBogie(List<String> list, String bogie) {
+        if (!list.contains(bogie)) {
+            list.add(bogie);
+        } else {
+            System.out.println("Duplicate bogie " + bogie + " not added.");
+        }
     }
 }
